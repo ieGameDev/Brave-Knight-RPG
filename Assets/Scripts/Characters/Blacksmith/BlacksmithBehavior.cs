52d1b6225606ca2df9a48bc39074a425c6c88982
Igor Eliseev <ie.game.develop@gmail.com>
2024-11-27 23:36:11 +0400

    new models

--- Assets/Scripts/Characters/Blacksmith/BlacksmithBehavior.cs
+++ Assets/Scripts/Characters/Blacksmith/BlacksmithBehavior.cs
@@ -5,7 +5,9 @@ namespace Characters.Blacksmith
     public class BlacksmithBehavior : MonoBehaviour
     {
         private static readonly int PlayerIsClose = Animator.StringToHash("PlayerIsClose");
+        
         [SerializeField] private Animator _animator;
+        [SerializeField] private ParticleSystem _particleSystem;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -15,6 +17,11 @@ namespace Characters.Blacksmith
         private void OnTriggerExit(Collider other)
         {
             _animator.SetBool(PlayerIsClose, false);
+        }
+
+        public void AnvilFX()
+        {
+            _particleSystem.Play();
         }
     }
 }
